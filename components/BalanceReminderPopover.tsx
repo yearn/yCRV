1379d6e730f1de6791d0dec9bc30552cfbfbae10
Majorfi <major-tom.eth@pm.me>
2022-09-26 13:34:35 +0200

    feat: add to MM

--- components/BalanceReminderPopover.tsx
+++ components/BalanceReminderPopover.tsx
@@ -1,12 +1,31 @@
 import React, {Fragment, ReactElement} from 'react';
 import {Popover, Transition} from '@headlessui/react';
-import {Wallet} from '@yearn-finance/web-lib/icons';
+import {AddToMetamask, Wallet} from '@yearn-finance/web-lib/icons';
 import {useWallet} from 'contexts/useWallet';
 import {format, toAddress} from '@yearn-finance/web-lib/utils';
 import Image from 'next/image';
+import {useWeb3} from '@yearn-finance/web-lib';
 
 export default function BalanceReminderPopover(): ReactElement {
 	const	{balances} = useWallet();
+	const	{provider} = useWeb3();
+
+	async function addTokenToMetamask(address: string, symbol: string, decimals: number, image: string): Promise<void> {
+		try {
+			// wasAdded is a boolean. Like any RPC method, an error may be thrown.
+			await provider.send('wallet_watchAsset', {
+				type: 'ERC20',
+				options: {
+					address,
+					symbol,
+					decimals,
+					image
+				}
+			});
+		} catch (error) {
+			// Token has not been added to MetaMask.
+		}
+	}
 
 	return (
 		<Popover className={'relative flex'}>
@@ -26,23 +45,35 @@ export default function BalanceReminderPopover(): ReactElement {
 					>
 						<Popover.Panel className={'absolute right-0 top-6 z-50 mt-3 w-screen max-w-[200px] md:top-4 md:-right-4 md:max-w-[280px]'}>
 							<div className={'overflow-hidden'}>
-								<div className={'border border-neutral-200 bg-neutral-100 p-0'}>
+								<div className={'bg-neutral-100 p-0'}>
 									<a
 										href={`https://etherscan.io/address/${process.env.YCRV_TOKEN_ADDRESS}`}
 										target={'_blank'} rel={'noreferrer'}
-										className={'flow-root px-3 py-2 transition-colors hover:bg-neutral-200 md:px-6 md:py-3'}>
+										className={'flow-root cursor-alias p-2 transition-colors hover:bg-neutral-200'}>
 										<span className={'flex flex-row items-center justify-between'}>
 											<span className={'flex items-center text-neutral-900'}>
 												<Image
 													alt={'yCRV'}
-													width={24}
-													height={24}
+													width={32}
+													height={32}
 													quality={90}
 													src={`${process.env.BASE_YEARN_ASSETS_URI}/1/${toAddress(process.env.YCRV_TOKEN_ADDRESS)}/logo-128.png`} />
 												<span className={'ml-2'}>{'yCRV'}</span>
 											</span>
-											<span className={'tabular-nums text-neutral-900'}>
+											<span className={'flex flex-row items-center justify-center tabular-nums text-neutral-900'}>
 												{format.amount(balances[toAddress(process.env.YCRV_TOKEN_ADDRESS)]?.normalized || 0, 2, 4)}
+												<AddToMetamask
+													onClick={(e): void => {
+														e.preventDefault();
+														e.stopPropagation();
+														addTokenToMetamask(
+															process.env.YCRV_TOKEN_ADDRESS as string,
+															'yCRV',
+															18,
+															`${process.env.BASE_YEARN_ASSETS_URI}/1/${toAddress(process.env.YCRV_TOKEN_ADDRESS)}/logo-128.png`
+														);
+													}}
+													className={'ml-4 h-4 w-4 cursor-pointer text-neutral-400 transition-colors hover:text-neutral-900'} />
 											</span>
 										</span>
 									</a>
@@ -50,19 +81,31 @@ export default function BalanceReminderPopover(): ReactElement {
 									<a
 										href={`https://etherscan.io/address/${process.env.STYCRV_TOKEN_ADDRESS}`}
 										target={'_blank'} rel={'noreferrer'}
-										className={'flow-root px-3 py-2 transition-colors hover:bg-neutral-200 md:px-6 md:py-3'}>
+										className={'flow-root cursor-alias p-2 transition-colors hover:bg-neutral-200'}>
 										<span className={'flex flex-row items-center justify-between'}>
 											<span className={'flex items-center text-neutral-900'}>
 												<Image
 													alt={'st-yCRV'}
-													width={24}
-													height={24}
+													width={32}
+													height={32}
 													quality={90}
 													src={`${process.env.BASE_YEARN_ASSETS_URI}/1/${toAddress(process.env.STYCRV_TOKEN_ADDRESS)}/logo-128.png`} />
 												<span className={'ml-2'}>{'st-yCRV'}</span>
 											</span>
-											<span className={'tabular-nums text-neutral-900'}>
+											<span className={'flex flex-row items-center justify-center tabular-nums text-neutral-900'}>
 												{format.amount(balances[toAddress(process.env.STYCRV_TOKEN_ADDRESS)]?.normalized || 0, 2, 4)}
+												<AddToMetamask
+													onClick={(e): void => {
+														e.preventDefault();
+														e.stopPropagation();
+														addTokenToMetamask(
+															process.env.STYCRV_TOKEN_ADDRESS as string,
+															'st-yCRV',
+															18,
+															`${process.env.BASE_YEARN_ASSETS_URI}/1/${toAddress(process.env.STYCRV_TOKEN_ADDRESS)}/logo-128.png`
+														);
+													}}
+													className={'ml-4 h-4 w-4 cursor-pointer text-neutral-400 transition-colors hover:text-neutral-900'} />
 											</span>
 										</span>
 									</a>
@@ -70,19 +113,31 @@ export default function BalanceReminderPopover(): ReactElement {
 									<a
 										href={`https://etherscan.io/address/${process.env.LPYCRV_TOKEN_ADDRESS}`}
 										target={'_blank'} rel={'noreferrer'}
-										className={'flow-root px-3 py-2 transition-colors hover:bg-neutral-200 md:px-6 md:py-3'}>
+										className={'flow-root cursor-alias p-2 transition-colors hover:bg-neutral-200'}>
 										<span className={'flex flex-row items-center justify-between'}>
 											<span className={'flex items-center text-neutral-900'}>
 												<Image
 													alt={'lp-yCRV'}
-													width={24}
-													height={24}
+													width={32}
+													height={32}
 													quality={90}
 													src={`${process.env.BASE_YEARN_ASSETS_URI}/1/${toAddress(process.env.LPYCRV_TOKEN_ADDRESS)}/logo-128.png`} />
 												<span className={'ml-2'}>{'lp-yCRV'}</span>
 											</span>
-											<span className={'tabular-nums text-neutral-900'}>
+											<span className={'flex flex-row items-center justify-center tabular-nums text-neutral-900'}>
 												{format.amount(balances[toAddress(process.env.LPYCRV_TOKEN_ADDRESS)]?.normalized || 0, 2, 4)}
+												<AddToMetamask
+													onClick={(e): void => {
+														e.preventDefault();
+														e.stopPropagation();
+														addTokenToMetamask(
+															process.env.LPYCRV_TOKEN_ADDRESS as string,
+															'lp-yCRV',
+															18,
+															`${process.env.BASE_YEARN_ASSETS_URI}/1/${toAddress(process.env.LPYCRV_TOKEN_ADDRESS)}/logo-128.png`
+														);
+													}}
+													className={'ml-4 h-4 w-4 cursor-pointer text-neutral-400 transition-colors hover:text-neutral-900'} />
 											</span>
 										</span>
 									</a>
