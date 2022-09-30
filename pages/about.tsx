398c1afdbd154aba570024eeb4dd12dc72482f07
Majorfi <major-tom.eth@pm.me>
2022-10-01 00:55:29 +0200

    fix: link

--- pages/about.tsx
+++ pages/about.tsx
@@ -17,7 +17,7 @@ function	About(): ReactElement {
 					<p className={' pb-4 text-neutral-600'}>{'We’ve completely overhauled our suite of curve products; refining, improving, and simplifying everything. The result? Our users get the highest yields, in the most streamlined way possible. Lfg.'}</p>
 					<p className={'text-neutral-600'}>
 						{'For more info on each token, and how to use the UI read our '}
-						<a href={'#'} target={'_blank'} className={'text-neutral-900 underline'} rel={'noreferrer'}>{'docs'}</a>
+						<a href={'https://docs.yearn.finance/getting-started/products/ycrv/overview'} target={'_blank'} className={'text-neutral-900 underline'} rel={'noreferrer'}>{'docs'}</a>
 						{'.'}
 					</p>
 				</div>
