ba4faf03a050c8ace0c781479ebbfc6c342a5757
Karelian Pie <devkarelianpie@gmail.com>
2022-10-11 18:31:38 +0300

    feat: Add new Zap-in tab

--- components/Header.tsx
+++ components/Header.tsx
@@ -45,6 +45,11 @@ function	Header(): ReactElement {
 							{'Vote'}
 						</p>
 					</Link> */}
+					<Link href={'/zap-in'}>
+						<p className={`yveCRV--nav-link ${router.pathname === '/zap-in' ? 'active' : '' }`}>
+							{'Zap-in'}
+						</p>
+					</Link>
 					<Link href={'/about'}>
 						<p className={`yveCRV--nav-link ${router.pathname === '/about' ? 'active' : '' }`}>
 							{'About'}
