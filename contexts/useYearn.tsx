0e7469412e9f997e71e1db490f26d15cec9cce8d
ai slave <6696080+ai-slave@users.noreply.github.com>
2022-11-21 12:06:44 +0000

    merge conflict glitch

--- contexts/useYearn.tsx
+++ contexts/useYearn.tsx
@@ -49,7 +49,7 @@ export const YearnContextApp = ({children}: {children: React.ReactElement}): Rea
 
 	const {settings} = useSettings();
 	const	{data: yveCRVdata} = useSWR(`${settings.apiBaseURI}/v1/chains/1/vaults/all`, fetcherLegacy);
-	const	{data: prices} = useSWR(`${settings.yDaemonBaseURI}/1/prices/some/${process.env.YCRV_TOKEN_ADDRESS},${process.env.YCRV_CURVE_POOL_ADDRESS},${process.env.CRV_TOKEN_ADDRESS}?humanized=true`, baseFetcher);
+	const	{data: prices} = useSWR(`${settings.yDaemonBaseURI}/1/prices/all`, baseFetcher);
 	const	{data} = useSWR(`${settings.yDaemonBaseURI}/1/vaults/all`, fetcher);
 	const	{data: yCRVHarvests} = useSWR(`${settings.yDaemonBaseURI}/1/vaults/harvests/${process.env.STYCRV_TOKEN_ADDRESS},${process.env.LPYCRV_TOKEN_ADDRESS}`, baseFetcher);
 
